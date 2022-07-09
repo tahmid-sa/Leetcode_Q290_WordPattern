b62e61ac9adb6dd3d6e14ef98ea005d52e6b96aa
Tahmid Sajin <tahmid.sajin.1999@gmail.com>
2022-07-09 02:02:09 -0400

    Adding files to github

---
+++ Q290_WordPattern/Q290_WordPattern.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+bool wordPattern(string pattern, string s) {
+    unordered_map<char, string> patterns;
+    unordered_set<string> words;
+    int i, j;
+
+    string str = "";
+    s += ' ';
+
+    for (i = 0, j = 0; i < s.size(); i++) {
+        if (s[i] == ' ') {
+            if (patterns.find(pattern[j]) != patterns.end()) {
+                if (patterns.find(pattern[j])->second == str) {
+                    j++;
+                    str.clear();
+                    continue;
+                }
+                else if (patterns.find(pattern[j])->second != str) {
+                    return false;
+                }
+            }
+            else if (patterns.find(pattern[j]) == patterns.end()) {
+                if (words.find(str) != words.end()) {
+                    return false;
+                }
+
+                patterns.insert(make_pair(pattern[j], str));
+                words.insert(str);
+            }
+
+            j++;
+            str.clear();
+        }
+        else if (s[i] != ' ') {
+            str += s[i];
+        }
+    }
+
+    if (j != pattern.size() || i != s.size()) {
+        return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    string pattern = "abba";
+    string s = "dog cat cat dog";
+
+    cout << wordPattern(pattern, s) << endl;
+
+    return 0;
+}
